f5b5e5c7bdea4319401a389c842e6492adf49f2a
Juan Carlos <Juancarmgc2017@gmail.com>
2023-05-17 12:35:26 +0200

    System con verdades

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -3,7 +3,9 @@ public class PrincipalApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Juan Antonio subnormal\n"
+				+ "Helen is angry\n"
+				+ "Antonio maricon\n");
 	}
 
 }
